84debbeaf0c8f5a40325b4bc3d5a29962bfd317d
“zebiniso” <szebiniso0909@gmail.com>
2023-11-03 18:02:25 +0600

    fixed component architechture

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import TextAreaInput from "./components/TextArea/TextAreaInput";
+import TextAnimation from "./components/TextAnimation/TextAnimation";
+import {useState} from "react";
 
 function App() {
+  const [value, setValue] = useState('Text something')
+  const [emoji, setEmoji] = useState([])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TextAnimation emojiList={emoji} value={value}/>
+      <TextAreaInput setEmoji={setEmoji} setValue={setValue}/>
     </div>
   );
 }
